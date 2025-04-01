6e96418ea85066012e4a15b816436cbf4d8af585
luq-mne <luqman.yazrie@outlook.com>
2025-04-01 17:54:48 +0800

    memory safe what is your name lol

--- main.cpp
+++ main.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
     cout << "the start of something i guess" << endl;
+
+    string* name = new string;
+
+    cout << "what is your name?" << endl;
+    cin >> *name;
+    cout << "hello " << *name << endl;
+    delete name;
+
     return 0;
 }
