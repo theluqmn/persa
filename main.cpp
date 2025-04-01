3676479a774c0f76fe578b3f506fe23a42befcdc
theluqmn <luqman.yazrie@outlook.com>
2025-04-01 19:19:40 +0800

    database initialisation now working, took longer than expected

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
+#include <sqlite3.h>
 
 using namespace std;
 
 void clear() {
-    system("cls");
+    system("clear");
 }
 
 void initDatabase() {
+    sqlite3* db;
+    char *errMsg = nullptr;
     
+    int rc = sqlite3_open("./finances.db", &db);
+    if (rc) {
+        cout << "Error opening database: " << sqlite3_errmsg(db) << endl;
+        sqlite3_close(db);
+        exit(1);
+    }
+
+    string* transactionTable = new string("CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, date TEXT, amount FLOAT, description TEXT)");
+    rc = sqlite3_exec(db, transactionTable->c_str(), nullptr, nullptr, &errMsg);
+    if (rc != SQLITE_OK) {
+        cout << "Error creating table: " << errMsg << endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        exit(1);
+    }
+
+    delete transactionTable;
+    sqlite3_close(db);
 }
 
 int main() {
     clear();
     cout << "the start of something i guess" << endl;
+
+    initDatabase();
 
     string* action = new string;
     bool exit = false;
